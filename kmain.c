bc72e10b9dd21d9e65029c80231d7c9fe8af87ed
Elliot Mitchum <elliotmitchum@gmail.com>
2019-04-27 09:17:28 +1000

    Fix alignment

--- kmain.c
+++ kmain.c
@@ -2,9 +2,9 @@
 #include "gdt.h"
 
 void kmain() {
-	 char message[] = "0x37";
-   gdt_init();
-	 fb_init();
-	 fb_frame_write_ln(message);
+  char message[] = "0x37";
+  gdt_init();
+  fb_init();
+  fb_frame_write_ln(message);
 }
 
